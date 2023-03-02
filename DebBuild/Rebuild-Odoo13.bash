d963bedcb604acbff2cb9aed62c2a4a7c9d6fb4c
João Jerónimo <joao@uniaolisboa-cgtp.pt>
2023-03-02 17:19:03 +0000

    Script postinst para restaurar o repositório da source do Odoo13.

--- DebBuild/Rebuild-Odoo13.bash
+++ DebBuild/Rebuild-Odoo13.bash
@@ -1,5 +1,7 @@
 
 # Odoo13:
 mkdir -p Odoo13/odoo/releases/13.0/
-cp -Rv /odoo/releases/13.0/.git Odoo13/odoo/releases/13.0/
+rsync -rlt --delete --itemize-changes                       \
+    cp -Rv /odoo/releases/13.0/.git                         \
+    Odoo13/odoo/releases/13.0/
 dpkg-deb --build --root-owner-group Odoo13/
--- DebBuild/Rebuild-Python.bash
+++ DebBuild/Rebuild-Python.bash
@@ -1,5 +1,7 @@
 
 # Python:
 mkdir -p Python-3.8.15-Odoo-Complete/odoo/RunTime/
-cp -Rv /odoo/RunTime/Python-3.8-install/ Python-3.8.15-Odoo-Complete/odoo/RunTime/
+rsync -rlt --delete --itemize-changes                       \
+    /odoo/RunTime/Python-3.8-install/                       \
+    Python-3.8.15-Odoo-Complete/odoo/RunTime/
 dpkg-deb --build --root-owner-group Python-3.8.15-Odoo-Complete/
