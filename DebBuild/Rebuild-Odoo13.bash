c7ee29d778abbf8dc2e134a4dc81cb54a9adc1e2
João Jerónimo <joao@uniaolisboa-cgtp.pt>
2023-03-15 11:02:49 +0000

    Adaptações.

--- DebBuild/Rebuild-Odoo13.bash
+++ DebBuild/Rebuild-Odoo13.bash
@@ -1,7 +1,5 @@
-
-# Odoo13:
-mkdir -p Odoo13/odoo/releases/13.0/
-rsync -rlt --delete --itemize-changes                       \
-    cp -Rv /odoo/releases/13.0/.git                         \
-    Odoo13/odoo/releases/13.0/
-dpkg-deb --build --root-owner-group Odoo13/
+export DEBNAME="Odoo13-Core"
+source lib_build_deb.bash
+echo "== Building DEB file for directory $DEBNAME . . ."
+rsynch_full_path /odoo/releases/13.0/odoo/ "$DEBNAME"/odoo/releases/13.0/
+dpkg-deb --build --root-owner-group "$DEBNAME"/
