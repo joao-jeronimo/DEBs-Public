fb2fd48cf298dd27715ac1974c9fc5839d56f887
Joao Jeronimo <joao.jeronimo.pro@gmail.com>
2023-03-26 22:30:12 +0000

    Puxar a pasta das libs para o PATH.

--- UseNet/DebBuild/RebuildDebs-UseNet.py
+++ UseNet/DebBuild/RebuildDebs-UseNet.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import os, glob, subprocess
+import sys, os, glob, subprocess
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+parentdir = os.path.join(scriptdir, os.path.pardir, os.path.pardir, "Libs")
+sys.path.insert(0, parentdir)
 from lib_build_deb import *
 
 NO_REBUILD = True
