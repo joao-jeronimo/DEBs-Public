8590b499823275f405a9ddfea07d487f9c5d7308
Joao Jeronimo <joao.jeronimo.pro@gmail.com>
2023-03-27 00:20:30 +0000

    Mais doc e cenas... Faltava o sidil depender do odoo...

--- AutoERP/DebBuild/RebuildDebs-SIDIL.py
+++ AutoERP/DebBuild/RebuildDebs-SIDIL.py
@@ -15,8 +15,10 @@ build_deb_from_installed_files(
     version         = "2023.03.23",
     maintainer      = "João Jerónimo <joao@uniaolisboa-cgtp.pt>",
     dependencies = [
-        "sudo", "git", "screenie",
         "venv-python38-odoo13-base", "venv-python38-odoo13-essentials", "venv-python38-odoo13-i18n", "venv-python38-odoo13-media", "venv-python38-odoo13-networking", "venv-python38-odoo13-tiny",
+        "odoo13-account", "odoo13-core", "odoo13-others", "odoo13-web",
+        
+        "sudo", "git", "screenie",
         "gpg", "lsb-release", "locate", "software-properties-common", "make", "gmsl", "nginx", "resolvconf",
         "libffi-dev", "libsasl2-dev", "libldap2-dev", "libssl-dev", "libpq-dev", "libjpeg-dev", "libxml2-dev", "libxslt-dev", "zlib1g-dev",
         "postgresql", "postgresql-client", "postgresql-contrib",
