8d0e6c8f6de3644d3d29686abe767266b1a19670
Joao Jeronimo <joao.jeronimo.pro@gmail.com>
2023-03-27 01:12:32 +0000

    Mais dep em falta.

--- AutoERP/DebBuild/RebuildDebs-SIDIL.py
+++ AutoERP/DebBuild/RebuildDebs-SIDIL.py
@@ -18,7 +18,7 @@ build_deb_from_installed_files(
         "venv-python38-odoo13-base", "venv-python38-odoo13-essentials", "venv-python38-odoo13-i18n", "venv-python38-odoo13-media", "venv-python38-odoo13-networking", "venv-python38-odoo13-tiny",
         "odoo13-account", "odoo13-core", "odoo13-others", "odoo13-web",
         
-        "sudo", "git", "screenie",
+        "sudo", "rsync", "git", "screenie",
         "gpg", "lsb-release", "locate", "software-properties-common", "make", "gmsl", "nginx", "resolvconf",
         "libffi-dev", "libsasl2-dev", "libldap2-dev", "libssl-dev", "libpq-dev", "libjpeg-dev", "libxml2-dev", "libxslt-dev", "zlib1g-dev",
         "postgresql", "postgresql-client", "postgresql-contrib",
